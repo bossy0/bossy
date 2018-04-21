71385dd8e6585e14be7b00543fc4d7cb52ca53e8
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-21 20:20:46 +0300

    Update HelloMyFriend.py

--- HelloMyFriend.py
+++ HelloMyFriend.py
@@ -22,16 +22,13 @@ else: # altrimenti
 print('''
 
 
-  hh   hh  iii  bbbbbbb  eeeeeee  rrrrrrrr  nn     nnn  eeeeeee  ttttttttt
-  hh   hh  iii  bb   bb  eeeeeee  rr    rr  nnn    nnn  eeeeeee     ttt
-  hh   hh  iii  bb   bb  ee       rr    rr  nnnn   nnn  ee          ttt
-  hh   hh  iii  bbbbbbb  ee       rrrrrrrr  nnnnnnnnnn  ee          ttt
-  hhhhhhh  iii  bb       eeeeeee  rrrr      nnnnnnnnnn  eeeeeee     ttt
-  hh   hh  iii  bbbbbbb  ee       rr rr     nnnnnnnnnn  ee          ttt
-  hh   hh  iii  bb   bb  ee       rr  rr    nn  nnnnnn  ee          ttt
-  hh   hh  iii  bb   bb  eeeeeee  rr   rr   nnn  nnnnn  eeeeeee     ttt
-  hh   hh  iii  bbbbbbb  eeeeeee  rr    rr  nnnn  nnnn  eeeeeee     ttt
-
+██████╗  ██████╗ ███████╗███████╗██╗   ██╗
+██╔══██╗██╔═══██╗██╔════╝██╔════╝╚██╗ ██╔╝
+██████╔╝██║   ██║███████╗███████╗ ╚████╔╝ 
+██╔══██╗██║   ██║╚════██║╚════██║  ╚██╔╝  
+██████╔╝╚██████╔╝███████║███████║   ██║   
+╚═════╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝   
+                                          
 
 							C0d3d by All3xJ
 	''') # la grafica ci sta
