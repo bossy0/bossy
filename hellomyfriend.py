b13290d508dd4cc4f7d5e2619d34846c5bdbf5af
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-21 13:01:58 +0300

    Update hellomyfriend.py

--- hellomyfriend.py
+++ hellomyfriend.py
@@ -338,7 +338,8 @@ in_file = open(raw_input("File Proxy ( proxy.txt ) : "),"r")
 proxyf = in_file.read()
 in_file.close()
 
-port=input("Port ( 80 ) : ')#Port we direct to attack
+port = input("Port ( 80 ) : ')
+port = input("Port ( 53 ) : ')#Port we direct to attack
 
 
 	   
