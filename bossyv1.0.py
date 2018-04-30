1892e35892b7895149e13beda2aa7d61e84d7b31
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-01 01:19:06 +0300

    Update bossyv1.0.py

--- bossyv1.0.py
+++ bossyv1.0.py
@@ -55,7 +55,7 @@ def down_it(item):
 				print("\033[91mshut<->down\033[0m")
 			time.sleep(.1)
 	except socket.error as e:
-		print("\033[91mno connection! server maybe down\033[0m")
+		print("\033[91mNo Connection ✓ Server Maybe Down\033[0m")
 		#print("\033[91m",e,"\033[0m")
 		time.sleep(.1)
 
