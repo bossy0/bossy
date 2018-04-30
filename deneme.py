bd133d5ffc44eda8b05b0d966daf26fbc0003faf
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-04-30 19:59:57 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,12 +1,4 @@
 import sys
-
-from colorama 
-import init
-init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
-from termcolor 
-import cprint 
-from pyfiglet 
-import figlet_format
-
-cprint(figlet_format('AkrepBey!', font='starwars'),
-       'yellow', 'on_red', attrs=['bold'])
+from pyfiglet import Figlet
+f = Figlet(font='slant')
+print f.renderText('text to rend')
