cb39d78793c9174abcf761d1fdedf98f03104d70
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-02 16:07:18 +0300

    Update bossyv1.0.py

--- bossyv1.0.py
+++ bossyv1.0.py
@@ -85,7 +85,7 @@ def down_it(item):
 			s.connect((host,int(port)))
 			if s.sendto( packet, (host, int(port)) ):
 				s.shutdown(1)
-				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m [*] Packet Sent %d AsparTim %  \033[0m")
+				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m [*] Packet Sent ✓ AsparTim  \033[0m")
 			else:
 				s.shutdown(1)
 				print("\033[91mshut<->down\033[0m")
