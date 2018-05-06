3b869a1d56c3fd46a1ba2ed586b95d78f02a189c
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-06 12:16:42 +0300

    Update bossyv2.0.py

--- bossyv2.0.py
+++ bossyv2.0.py
@@ -7,16 +7,16 @@ import sys
 import time
 
 
-parser = argparse.ArgumentParser(description="Bossyv2, low bandwidth stress test tool for websites")
-parser.add_argument('host', nargs="?", help="Host to perform stress test on")
-parser.add_argument('-p', '--port', default=80, help="Port of webserver, usually 80", type=int)
-parser.add_argument('-s', '--sockets', default=150, help="Number of sockets to use in the test", type=int)
-parser.add_argument('-v', '--verbose', dest="verbose", action="store_true", help="Increases logging")
-parser.add_argument('-ua', '--randuseragents', dest="randuseragent", action="store_true", help="Randomizes user-agents with each request")
-parser.add_argument('-x', '--useproxy', dest="useproxy", action="store_true", help="Use a SOCKS5 proxy for connecting")
-parser.add_argument('--proxy-host', default="127.0.0.1", help="SOCKS5 proxy host")
-parser.add_argument('--proxy-port', default="8080", help="SOCKS5 proxy port", type=int)
-parser.add_argument("--https", dest="https", action="store_true", help="Use HTTPS for the requests")
+parser = argparse.ArgumentParser(description="Bossyv2 Low Bandwidth Stress Test Tool For Websites")
+parser.add_argument('host', nargs="?", help="Host To Perform Stress Test On")
+parser.add_argument('-p', '--port', default=80, help="Port Of Webserver Usually 80", type=int)
+parser.add_argument('-s', '--sockets', default=150, help="Number Of Sockets To Use İn The Test", type=int)
+parser.add_argument('-v', '--verbose', dest="verbose", action="store_true", help="Increases Logging")
+parser.add_argument('-ua', '--randuseragents', dest="randuseragent", action="store_true", help="Randomizes User-Agents With Each Request")
+parser.add_argument('-x', '--useproxy', dest="useproxy", action="store_true", help="Use A SOCKS5 Proxy For Connecting")
+parser.add_argument('--proxy-host', default="127.0.0.1", help="SOCKS5 Proxy Host")
+parser.add_argument('--proxy-port', default="8080", help="SOCKS5 Proxy Host", type=int)
+parser.add_argument("--https", dest="https", action="store_true", help="Use HTTPS For The Requests")
 parser.set_defaults(verbose=False)
 parser.set_defaults(randuseragent=False)
 parser.set_defaults(useproxy=False)
@@ -76,7 +76,7 @@ if len(sys.argv) <= 1:
     sys.exit(1)
 
 if not args.host:
-    print("Host Required !")
+    print("\033[91m [*] Host Required ! \033")
     parser.print_help()
     sys.exit(1)
 
@@ -88,9 +88,9 @@ if args.useproxy:
         import socks
         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port)
         socket.socket = socks.socksocket
-        logging.info("Using SOCKS5 Proxy For Connecting...")
+        logging.info("\033[91m [*]  Using SOCKS5 Proxy For Connecting . . . \033")
     except ImportError:
-        logging.error("Socks Proxy Library Not Available!")
+        logging.error("\033[91m [*] Socks Proxy Library Not Available ! \033")
 
 if args.verbose:
     logging.basicConfig(format="\033[92m[%(asctime)s] %(message)s\033", datefmt="%d-%m-%Y %H:%M:%S", level=logging.DEBUG)
