0f73186f05dc3535607e89c889c777fbda0bea1a
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-06 11:30:46 +0300

    Update bossyv2.0.py

--- bossyv2.0.py
+++ bossyv2.0.py
@@ -7,6 +7,22 @@ import sys
 import time
 
 
+parser = argparse.ArgumentParser(description="Bossyv2, low bandwidth stress test tool for websites")
+parser.add_argument('host', nargs="?", help="Host to perform stress test on")
+parser.add_argument('-p', '--port', default=80, help="Port of webserver, usually 80", type=int)
+parser.add_argument('-s', '--sockets', default=150, help="Number of sockets to use in the test", type=int)
+parser.add_argument('-v', '--verbose', dest="verbose", action="store_true", help="Increases logging")
+parser.add_argument('-ua', '--randuseragents', dest="randuseragent", action="store_true", help="Randomizes user-agents with each request")
+parser.add_argument('-x', '--useproxy', dest="useproxy", action="store_true", help="Use a SOCKS5 proxy for connecting")
+parser.add_argument('--proxy-host', default="127.0.0.1", help="SOCKS5 proxy host")
+parser.add_argument('--proxy-port', default="8080", help="SOCKS5 proxy port", type=int)
+parser.add_argument("--https", dest="https", action="store_true", help="Use HTTPS for the requests")
+parser.set_defaults(verbose=False)
+parser.set_defaults(randuseragent=False)
+parser.set_defaults(useproxy=False)
+parser.set_defaults(https=False)
+args = parser.parse_args()
+
 print (" ")
 print (''' \033[91m [*] Loading , Please Wait . . . \033[0m''')
 print (" ")
