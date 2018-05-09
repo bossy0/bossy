16ab8e96f0da9a9bd636dfd0826d8929d4a21d48
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-09 18:39:28 +0300

    Update bossy.c

--- bossy.c
+++ bossy.c
@@ -34,12 +34,12 @@ int make_socket(char *host, char *port) {
 	if(p == NULL) {
 		if(servinfo)
 			freeaddrinfo(servinfo);
-		fprintf(stderr, "[!] Please Enter Site Address\n");
+		fprintf(stderr, "\033[92m [!] Please Enter Site Address\n \033[0m");
 		exit(0);
 	}
 	if(servinfo)
 		freeaddrinfo(servinfo);
-	fprintf(stderr, "[*] Connected ✓ %s:%s\n", host, port);
+	fprintf(stderr, "\033[92m [*] Connected ✓ %s:%s\n \033[0m", host, port);
 	return sock;
 }
 
@@ -66,9 +66,9 @@ void attack(char *host, char *port, int id) {
 				sockets[x] = make_socket(host, port);
 			} else
 //				fprintf(stderr, "Socket[%i->%i] -> %i\n", x, sockets[x], r);
-			fprintf(stderr, "[%i: Voly Sent]\n", id);
+			fprintf(stderr, "\033[92m [%i: Voly Sent]\n \033[0m", id);
 		}
-		fprintf(stderr, "[%i: Voly Sent]\n", id);
+		fprintf(stderr, "\033[92m [%i: Voly Sent]\n \033[0m", id);
 		usleep(300000);
 	}
 }
