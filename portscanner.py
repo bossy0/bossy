7feb2166aea4d4eeaef83a9bbce4ffe39ab6ff8b
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-11 21:22:42 +0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -29,7 +29,11 @@ def scan(ports):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((remoteServerIP, ports))
     if result == 0:
-    print "[*] Port {} : 	Open".format(ports)
+        byte = str.encode("Server:\r\n")
+        sock.send(byte)
+        banner = sock.recv(1024)
+    print "[*] Port {} : 	Open".format(ports), " - ", banner
+    sock.close()
 
 # function to be mapped over
 def scanParallel(ports, threads=4):
