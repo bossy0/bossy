8e355324924ce92172707fffe41843a104766239
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-12 21:24:21 +0300

    Update pscan.py

--- pscan.py
+++ pscan.py
@@ -23,7 +23,7 @@ def t():
     return "["+ ctime + "]"
 def shutdown():
 	print ""
-	print bcolors.BGRED + bcolors.WHITE + t() + "[info] shutting down CPSCAN" + bcolors.ENDC +"\n\n"
+	print bcolors.BGRED + bcolors.WHITE + t() + "[info] Shutting Down PSCAN" + bcolors.ENDC +"\n\n"
 	sys.exit()
 def usage():
     print bcolors.RED + bcolors.BOLD
@@ -36,12 +36,12 @@ def usage():
     print bcolors.ENDC
     print """
         USAGE:
-         -t  --target   - Target web server "www.example.com"
-         -v  --verbose  - Enable verbose mode
-         -h  --help     - show this menu
+         -t  --target   - Target Web Server "www.example.com"
+         -v  --verbose  - Enable Verbose Mode
+         -h  --help     - Show This Menu
 
         EXAMPLE:
-          python pscan.py -t www.targetsite.com -v
+          python2 pscan.py -t www.targetsite.com -v
     """
     sys.exit()
 
@@ -55,9 +55,9 @@ def check(host, path):
 
 
 def final_result():
-    print t() + "[info] scan complete"
+    print t() + "[info] Scan Complete"
     if len(result_array)==0:
-        print bcolors.RED + bcolors.BOLD + t() + "[critical] sorry! CPSCAN could not find any possible directories" + bcolors.ENDC
+        print bcolors.RED + bcolors.BOLD + t() + "[critical] Sorry! PSCAN Could Not Find Any Possible Directories" + bcolors.ENDC
     else:
         print bcolors.GREEN + bcolors.BOLD
         print t() + "[info] Found " + str(len(result_array)) + " possible directory"
@@ -99,30 +99,30 @@ print """
 """
 print bcolors.ENDC
 
-print t() + "[info] Checking connection to target server"
+print t() + "[info] Checking Connection To Target Server"
 
 
 ccode = check(host,"/")
 
 if (ccode < 400):
-	print bcolors.BOLD + t() + "[info] Target server is up and running" + bcolors.ENDC
+	print bcolors.BOLD + t() + "[info] Target Server is Up And Running" + bcolors.ENDC
 else:
-	print bcolors.RED + bcolors.BOLD + t() + "[warning] Target server seems to be down. check your internet connection or proxy settings. see misspelled words if any " + bcolors.ENDC
+	print bcolors.RED + bcolors.BOLD + t() + "[warning] Target Server Seems To Be Down. Check Your internet Connection Or Proxy Settings. See Misspelled Words if Any " + bcolors.ENDC
 	shutdown()
 
-print t() + "[info] Initiating scan loading directory list"
+print t() + "[info] Initiating Scan Loading Directory List"
 
 f = open( "panelist.txt", "r" )
 directory = []
 for line in f:
     directory.append(line)
 
-print t() + "[info] Ruinning directory scan to the target server."
+print t() + "[info] Ruinning Directory Scan To The Target Server."
 maxlen=len(directory)
 if (v==0):
-    print t() + "[info] "+ str(maxlen) + " Directories loaded This may take a while pls wait.. use option -v for verbose mode"
+    print t() + "[info] "+ str(maxlen) + " Directories Loaded This May Take A While Pls Wait.. Use Option [-v] For Verbose Mode"
 else:
-    print t() + "[info] "+ str(maxlen) + " Directories loaded This may take a while pls wait.."
+    print t() + "[info] "+ str(maxlen) + " Directories Loaded This May Take A While Pls Wait.."
 i=0
 result_array = []
 for i in range (maxlen):
@@ -139,7 +139,7 @@ for i in range (maxlen):
         result="[response]" + "["+code+"]" +" =>  "+ host + c_dir
         result_array.insert(num,result)
         num = num+1
-        reply = str(raw_input(bcolors.BOLD +' Do you want to continue scan for more possible results? (y/n): ')).lower().strip()
+        reply = str(raw_input(bcolors.BOLD +' Do You Want To Continue Scan For More Possible Results ? (Y/N) : ')).lower().strip()
         print bcolors.ENDC
         if (reply == 'n'):
             final_result()
