b5d9ffe323ceba09349c351971329d7841ce0418
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-22 03:09:39 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -99,10 +99,10 @@ if __name__ == '__main__':
 			ip = raw_input("> Enter IP : "); port = input("> Port No : ")
 			synFlood(ip, port)
 		elif option == 3: # TCP FLOOD
-			ip = raw_input("Enter IP: "); port = input("Port No: "); size = input("Size: ")
+			ip = raw_input("> Enter IP : "); port = input("> Port No : "); size = input("> Size : ")
 			tcpFlood(ip, port, size)
 		elif option == 4: # UDP FLOOD
-			ip = raw_input("Enter IP: "); port = input("Port No: "); size = input("Size: ")
+			ip = raw_input("> Enter IP : "); port = input("> Port No : "); size = input("Size: ")
 			udpFlood(ip, port, size)
 
 	except KeyboardInterrupt:
