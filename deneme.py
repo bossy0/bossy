a22146720bebe2eb7d605d4382508d4c41bfd3f5
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-05-25 16:58:41 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -248,7 +248,7 @@ def inforgeget(): # anche questa funzione scarica proxy pero' da inforge.net
 
 def proxylist():
 	global proxies
-	out_file = str(input("[+] Please 'proxy.txt' Write: "))
+	out_file = str(input("[+] Please 'proxy.txt' Write : "))
 	if out_file == "":
 		out_file = "proxy.txt"
 	proxies = open(out_file).readlines()
