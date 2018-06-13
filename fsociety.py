b27e4206b9e4d333a28ce3b159d6f45decd80bc0
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-06-13 21:18:12 +0300

    Update fsociety.py

--- fsociety.py
+++ fsociety.py
@@ -565,7 +565,7 @@ class requestproxy(threading.Thread): # la classe del multithreading
 				s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # ecco il nostro socket
 				s.connect((str(proxy[0]), int(proxy[1]))) # connessione al proxy
 				s.send(str.encode(request)) # encode in bytes della richiesta HTTP
-				print (''' \033[91m " " + str(proxy[0]+":"+proxy[1]) + " ", self.counter) \033[0m''') # print delle richieste
+				print (''' \033[91m " " + str(proxy[0]+":"+proxy[1]) + " ", self.counter \033[0m''') # print delle richieste
 				try: # invia altre richieste nello stesso thread
 					for y in range(multiple): # fattore di moltiplicazione
 						s.send(str.encode(request)) # encode in bytes della richiesta HTTP
