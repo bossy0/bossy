5066d60fa3f21b48a84bdfdd6988308c77771da0
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-08-22 01:40:25 +0300

    Update germ.py

--- germ.py
+++ germ.py
@@ -590,7 +590,7 @@ def main():
             except EnvironmentError:
                     error("cannot read file {0}".format(uas_file))
 
-        goldeneye = GoldenEye(url)
+        GERM = GERM(url)
         GERM.useragents = useragents
         GERM.nr_workers = workers
         GERM.method = method
