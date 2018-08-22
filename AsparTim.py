b9241d4b9ea07129436698725b70c3292eedea68
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-08-22 21:06:45 +0300

    Update AsparTim.py

--- AsparTim.py
+++ AsparTim.py
@@ -506,7 +506,7 @@ class Striker(Process):
 
 def usage():
 	print (''' \033[91m              AsparTim Dos Script v.1
-	print
+	
   ____  _____ ____   ____  ____  ______  ____  ___ ___ 
  /    |/ ___/|    \ /    ||    \|      ||    ||   |   |
 |  o  (   \_ |  o  )  o  ||  D  )      | |  | | _   _ |
