9d45063f2b9859b9765e556a7cbf49d1a8d48e2d
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-25 22:27:46 +0300

    Update bossyv1.0.py

--- bossyv1.0.py
+++ bossyv1.0.py
@@ -13,6 +13,28 @@ import random
 import time
 import sys
 
+with tqdm(total = 100) as progressbar:
+	for i in range(10):
+		sleep(0.1)
+		progressbar.update(10)
+
+
+progressbar = tqdm([2,4,6,8,10,12,14,16])
+
+for item in progressbar:
+	sleep(0.1)
+	progressbar.set_description('Processing element: {}'.format(item))
+
+
+for i in trange(20):
+	sleep(0.1)
+	pass
+
+
+for i in tqdm(range(20)):
+	sleep(0.5)
+	pass
+
 class colors:
   W  = '\033[0m'  # white (default)
   R  = '\033[31m' # red
