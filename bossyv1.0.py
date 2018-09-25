637d26ad6e6e9cf13bdec1eda0021a557431e82b
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-25 22:57:38 +0300

    Update bossyv1.0.py

--- bossyv1.0.py
+++ bossyv1.0.py
@@ -194,6 +194,9 @@ print(Ghost_Lab.End_Status)
 time.sleep(2)
 
 def usage():
+		print (''' \033[92m	Hammer Dos Script v.1 http://www.canyalcin.com/
+	It is the end user's responsibility to obey all applicable laws.
+	It is just for server testing script. Your ip is visible. \n
 	usage : python3 bossyv1.0.py [-s] [-p] [-t]
 	-h : help
 	-s : server ip
