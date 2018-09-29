37e2df9ed854e352199f57e812e7a2b393f940a8
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-29 19:17:24 +0300

    Update TCPDoser.py

--- TCPDoser.py
+++ TCPDoser.py
@@ -3,10 +3,10 @@ import socket
 import random
 import threading
 
-print " "
-print " ╔╦╗╔═╗╔═╗  ╔╦╗╔═╗╔═╗╔═╗╦═╗ "
-print "  ║ ║  ╠═╝───║║║ ║╚═╗║╣ ╠╦╝ "
-print "  ╩ ╚═╝╩    ═╩╝╚═╝╚═╝╚═╝╩╚═ "
+print (" ")
+print (" ╔╦╗╔═╗╔═╗  ╔╦╗╔═╗╔═╗╔═╗╦═╗ ")
+print ("  ║ ║  ╠═╝───║║║ ║╚═╗║╣ ╠╦╝ ")
+print ("  ╩ ╚═╝╩    ═╩╝╚═╝╚═╝╚═╝╩╚═ ")
 time.sleep(0.5)
 print (" ")
 ip = raw_input("IP : ")
