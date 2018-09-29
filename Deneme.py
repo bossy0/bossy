6a9da3fcbe5c04df316b58a0b50f729cd794760e
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-29 18:18:39 +0300

    Update Deneme.py

--- Deneme.py
+++ Deneme.py
@@ -10,7 +10,7 @@ print (" ")
 
 user_agent = [
   "Mozilla/5.0 (Linux; Android 8.0.0; GM 5 Plus d Build/OSR18H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36"
-       ]
+       
 
 def run():
 	if len(sys.argv)>=1:
