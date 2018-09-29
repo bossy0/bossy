49af2a032c977631951a3f3dd2d111c48af1dfd2
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-09-29 19:42:23 +0300

    Update TCPDoser.py

--- TCPDoser.py
+++ TCPDoser.py
@@ -6,7 +6,7 @@ import time
 ip = raw_input("IP : ")
 port = input("Port : ")
 thread_num = input("Threads : ")
-print " "
+thread_num = 1000
 print "Please Wait While Packages Are Preparing Thread :",thread_num
 time.sleep(5)
 
